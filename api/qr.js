1092abeea18d8e1706510b16f7b4f6e774feeb24
Ксено <aldoaldo0432@gmail.com>
2026-07-23 23:27:42 +0700

    qr.js

--- api/qr.js
+++ api/qr.js
@@ -1,20 +1,74 @@
 const QRCode = require("qrcode");
 
-module.exports = async (req,res)=>{
+module.exports = async (req, res) => {
 
-const text=req.query.text;
+const text = req.query.text;
 
 if(!text){
+
 return res.status(400).json({
+
 creator:"Xeno",
+
 status:false,
-message:"Text kosong"
+
+message:"Text atau URL kosong"
+
 });
+
 }
 
-const qr=await QRCode.toBuffer(text);
 
-res.setHeader("Content-Type","image/png");
+try{
+
+
+const qr = await QRCode.toBuffer(text, {
+
+type:"png",
+
+width:500,
+
+margin:2,
+
+color:{
+dark:"#000000",
+light:"#ffffff"
+}
+
+});
+
+
+res.setHeader(
+"Content-Type",
+"image/png"
+);
+
+
+res.setHeader(
+"Content-Disposition",
+"inline; filename=xapi-qr.png"
+);
+
+
 res.send(qr);
+
+
+
+}catch(error){
+
+
+res.status(500).json({
+
+creator:"Xeno",
+
+status:false,
+
+message:error.message
+
+});
+
+
+}
+
 
 };
